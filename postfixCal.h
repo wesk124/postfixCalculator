a1bd05a471366383b46b2a8d5e50ab2948be6990
wesk124 <capwone@gmail.com>
2015-11-05 00:22:16 -0500

    rewrite a stack and implementation function

--- postfixCal.h
+++ postfixCal.h
@@ -8,33 +8,11 @@
 #include <stdio.h>
 
 
-typedef char *stackEle;
-
-typedef struct myStack *stack;
 
 
-typedef struct myStack
-{
-    int stack_size;
-    int top_of_stack; // The index of the array;
-    stackEle *array; // The array used for storing the elements of stack
-} myStack;
 
-stack initStack(int size);
 
-void destroyStack(stack S);
-
-int isEmpty();
-
-int isFull();
-
-int push(stackEle input, stack S);
-
-int pop(stack S);
-
-stackEle peek(stack S);
 
 
 
 #endif
-
